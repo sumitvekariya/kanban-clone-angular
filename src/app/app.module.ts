d173da6d8daafba82fb6bb347365e3eb0643acee
Sumit Vekariya <sumitvekariya7@gmail.com>
2021-12-09 11:28:41 +0530

    kanban clone angular

---
+++ src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { environment } from '../environments/environment';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// App Modules
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HomePageComponent } from './home-page/home-page.component';
+import { SharedModule } from './shared/shared.module';
+import { UserModule } from './user/user.module';
+
+// Firebase imports
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+// import { ServiceWorkerModule } from '@angular/service-worker';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomePageComponent,
+  ],
+  imports: [
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    SharedModule,
+    UserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    AngularFireAuthModule,
+    // ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyC3wrWSidTsUgqMf8FKl_5KD4A2ZdQ4aWI",
+    authDomain: "trello-clone-de4af.firebaseapp.com",
+    projectId: "trello-clone-de4af",
+    storageBucket: "trello-clone-de4af.appspot.com",
+    messagingSenderId: "479231523317",
+    appId: "1:479231523317:web:4b06e9a5a0d4be0275c8bc"
+  }
+};
